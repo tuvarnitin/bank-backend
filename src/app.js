56c9ca0d67d5813f9d9669d15481cbe408b5fd92
tuvarnitin <nitintuvar2003@gmail.com>
2026-03-01 19:36:21 +0530

    .

--- src/app.js
+++ src/app.js
@@ -1,20 +1,14 @@
 const express = require("express")
 require("dotenv").config()
 
-const app = express()
-
+const authRoute = require("./routes/auth.route")
 const connectToDB = require("./config/db.config")
 
-const authRoute = require("./routes/auth.route")
+const app = express()
 
-
+connectToDB()
 app.use(express.json())
 
 app.use("/api/auth",authRoute)
-
-connectToDB()
-
-
-
 
 module.exports = app
