75d113db19f2852a16947786b10037755e940776
tuvarnitin <nitintuvar2003@gmail.com>
2026-03-02 16:25:37 +0530

    Added cookie-parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 require("dotenv").config()
+const cookieParser = require("cookie-parser")
 
 const authRoute = require("./routes/auth.route")
 const connectToDB = require("./config/db.config")
@@ -8,6 +9,7 @@ const app = express()
 
 connectToDB()
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth",authRoute)
 
